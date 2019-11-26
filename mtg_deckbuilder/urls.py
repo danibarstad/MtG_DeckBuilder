6a38c22a4d0bd2b9e55c12fbadf46f912de90302
Dani <dani@dani.dani>
2019-11-25 22:53:20 -0600

    more user profile additions, logging in and out, etc

--- mtg_deckbuilder/urls.py
+++ mtg_deckbuilder/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views, views_decks, views_users
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
     path('', views.homepage, name='homepage'),
@@ -12,5 +14,10 @@ urlpatterns = [
     path('user/profile/<int:user_pk>/', views_users.user_profile, name='user_profile'),
     path('user/profile/', views_users.my_user_profile, name='my_user_profile'),
     path('user/profile/edit', views_users.edit_user_profile, name='edit_user_profile'),
-    path('user/password/', views_users.change_password, name='change_password')
+    path('user/password/', views_users.change_password, name='change_password'),
+
+    # Account
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views_users.register, name='register')
 ]
--- mtg_deckbuilder/views_users.py
+++ mtg_deckbuilder/views_users.py
@@ -6,7 +6,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-# from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
+
 
 
 def user_profile(request, user_pk):
