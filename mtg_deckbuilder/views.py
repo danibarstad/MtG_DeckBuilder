d89797638ef28415edc4d8268cf1f65889ad69b7
Dani <dani@dani.dani>
2019-12-07 13:46:13 -0600

    all decks in database now display on homepage

--- mtg_deckbuilder/views.py
+++ mtg_deckbuilder/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Deck
 
 
 def homepage(request):
-    return render(request, 'mtg_deckbuilder/home.html')
+    decks = Deck.objects.all()
+    return render(request, 'mtg_deckbuilder/home.html', {'decks': decks})
+
+
+def deck_detail(request, deck_pk):
+    deck = get_object_or_404(Deck, pk=deck_pk)
+    return render(request, 'decks/deck_detail.html' , {'deck' : deck })
